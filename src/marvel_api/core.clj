cd9b5b0d7d5f4c3f6ffa521f88a156d1f8fba779
jffjs <jffreyjs@gmail.com>
2014-05-22 17:47:13 -0400

    add set-credentials

--- src/marvel_api/core.clj
+++ src/marvel_api/core.clj
@@ -1,24 +1,20 @@
 (ns marvel-api.core
   (:require digest [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare public-key private-key)
+(def base-url "http://gateway.marvel.com/")
 
-(defn timestamp
+(defn- timestamp
   "Generates a timestamp for use in requests."
   []
   (System/currentTimeMillis))
 
-(defn md5-hash
+(defn- md5-hash
   "Concat arguments and hash using md5."
   [& args]
   (digest/md5 (apply str args)))
 
-(def base-url "http://gateway.marvel.com/")
-
-(defn get-response
+(defn- get-response
   [method public-key private-key]
   (let [ts (timestamp)]
       (client/get (str base-url "v1/public/" (name method))
@@ -26,6 +22,12 @@
                    :query-params {"ts" ts
                                   "hash" (md5-hash ts private-key public-key)
                                   "apikey" public-key}})))
+(defn set-credentials
+  [public-key private-key]
+  (do
+    (def public-key public-key)
+    (def private-key private-key)))
+
 (defn get-comics
   [public-key private-key]
   (get-response :comics public-key private-key))
