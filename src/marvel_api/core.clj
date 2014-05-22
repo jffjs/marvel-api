be8cc86c3f7e934b4ee05b74b0f3a8354c4fa439
Jeff Smith <jffreyjs@gmail.com>
2014-05-21 22:04:20 -0400

    get characters

--- src/marvel_api/core.clj
+++ src/marvel_api/core.clj
@@ -18,11 +18,18 @@
 
 (def base-url "http://gateway.marvel.com/")
 
+(defn get-response
+  [method public-key private-key]
+  (let [ts (timestamp)]
+      (client/get (str base-url "v1/public/" (name method))
+                  {:as :json
+                   :query-params {"ts" ts
+                                  "hash" (md5-hash ts private-key public-key)
+                                  "apikey" public-key}})))
 (defn get-comics
   [public-key private-key]
-  (let [ts (timestamp)]
-    (client/get (str base-url "v1/public/comics")
-                {:as :json
-                 :query-params {"ts" ts
-                                "hash" (md5-hash ts private-key public-key)
-                                "apikey" public-key}})))
+  (get-response :comics public-key private-key))
+
+(defn get-characters
+  [public-key private-key]
+  (get-response :characters public-key private-key))
