221562805e05a3730880ae9bb8a9308a1d0eeaf9
Jeff Smith <jffreyjs@gmail.com>
2014-06-06 22:01:24 -0400

    trying out midje

--- test/marvel_api/core_test.clj
+++ test/marvel_api/core_test.clj
@@ -1,9 +1,17 @@
 (ns marvel-api.core-test
-  (:use midje.sweet)
+  (:use midje.sweet
+        [midje.util :only [testable-privates]])
   (:require [marvel-api.core :refer :all]))
 
-(facts "`md5-hash`"
-       (fact "it concats it's arguments and applies md5 hash"
-             (md5-hash "foo") => "acbd18db4cc2f85cedef654fccc4a4d8"
-             (md5-hash "foo" "bar") => "3858f62230ac3c915f300c664312c63f"
-             (md5-hash "foo" "bar" "qaaz") => "014c410e8ca5ac3c30b695192ea3a3d3"))
+(testable-privates marvel-api.core md5-hash timestamp)
+
+(fact "`md5-hash` concats its arguments and applies md5 hash"
+      (println )
+     (md5-hash "foo") => "acbd18db4cc2f85cedef654fccc4a4d8"
+     (md5-hash "foo" "bar") => "3858f62230ac3c915f300c664312c63f"
+     (md5-hash "foo" "bar" "qaaz") => "014c410e8ca5ac3c30b695192ea3a3d3")
+
+(fact "`set-credentials` stores public-key and private-key for later calls"
+      (set-credentials "publickey" "privatekey")
+      public-key => "publickey"
+      private-key => "privatekey")
