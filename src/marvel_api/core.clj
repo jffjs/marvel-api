b7fc21cf3d16bcc99a4cfcfb8c1c92dbd78dc70e
Jeff Smith <jffreyjs@gmail.com>
2014-06-08 09:49:55 -0400

    trying out different ways of resource representation

--- src/marvel_api/core.clj
+++ src/marvel_api/core.clj
@@ -15,9 +15,9 @@
   (digest/md5 (apply str args)))
 
 (defn- get-response
-  [method params public-key private-key]
+  [url params public-key private-key]
   (let [ts (timestamp)]
-      (client/get (str base-url "v1/public/" (name method))
+      (client/get (str base-url "v1/public/" url)
                   {:as :json
                    :query-params (merge params {"ts" ts
                                                 "hash" (md5-hash ts private-key public-key)
@@ -28,9 +28,55 @@
     (def public-key public-key)
     (def private-key private-key)))
 
-(defn get-comics
-  [params public-key private-key]
-  (get-response :comics params public-key private-key))
+(defn get-resource
+  ([url]
+   (get-resource url {} public-key private-key))
+  ([url params]
+   (get-resource url params public-key private-key))
+  ([url params public-key private-key]
+    (get-in (get-response url params public-key private-key) [:body :data])))
+
+(defn resource-fn
+  ([url]
+   (resource-fn url {} public-key private-key))
+  ([url params]
+   (resource-fn url params public-key private-key))
+  ([url params public-key private-key]
+   (fn [new-params]
+     (get-resource url (merge params new-params) public-key private-key))))
+
+(defn lazy-collection
+  "Returns a lazy seq for the given resource collection"
+  ([rsc-fn]
+   (lazy-collection rsc-fn nil))
+  ([rsc-fn params]
+   (lazy-seq
+    (let [data (rsc-fn params) s (:results data)]
+      (if-not (empty? s)
+        (concat s (lazy-collection rsc-fn {:offset (+ (:offset data) (:limit data))})))))))
+
+(defn characters
+  "Constructs url for Characters resource"
+  ([]
+   "characters")
+  ([id]
+   (str (characters) "/" id))
+  ([id nested-rsc]
+   (str (characters id) "/" (nested-rsc))))
+
+(defn comics
+  "Constructs url for Comics resource"
+  ([]
+   "comics")
+  ([id]
+   (str (comics) "/" id))
+  ([id nested-rsc]
+   (str (comics id) "/" (nested-rsc))))
+
+(defn resource
+  "Returns a resource"
+  [rsc-type params]
+  (get-in (get-response rsc-type (merge params {:limit 100}) public-key private-key) [:body :data]))
 
 (defn lazy-comics
   ([]
--- test/marvel_api/core_test.clj
+++ test/marvel_api/core_test.clj
@@ -11,7 +11,29 @@
      (md5-hash "foo" "bar") => "3858f62230ac3c915f300c664312c63f"
      (md5-hash "foo" "bar" "qaaz") => "014c410e8ca5ac3c30b695192ea3a3d3")
 
-(fact "`set-credentials` stores public-key and private-key for later calls"
-      (set-credentials "publickey" "privatekey")
-      public-key => "publickey"
-      private-key => "privatekey")
+;; There's probably a better way I should do this
+;; public-key and private-key get changed everytime test runs
+;(with-state-changes [(before :facts (do (def tmp-public-key public-key)
+                                        ;(def tmp-private-key private-key)))
+                     ;(after :facts (do (def public-key tmp-public-key)
+                                       ;(def private-key tmp-private-key)))]
+  ;(fact "`set-credentials` stores public-key and private-key for later calls"
+        ;(set-credentials "publickey" "privatekey")
+        ;public-key => "publickey"
+        ;private-key => "privatekey"))
+
+(facts "`characters` constructs url representation of "
+       (fact "collection"
+             (characters) => "characters")
+       (fact "single resource"
+             (characters 12345) => "characters/12345")
+       (fact "nested resource collections"
+             (characters 12345 comics) => "characters/12345/comics"))
+
+(facts "`comics` constructs url representation of"
+       (fact "collection"
+             (comics) => "comics")
+       (fact "single resource"
+             (comics 123) => "comics/123")
+       (fact "nested resource collections"
+             (comics 123 characters) = "comics/123/characters"))
